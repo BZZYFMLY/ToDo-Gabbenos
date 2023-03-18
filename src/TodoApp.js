4ba1520d5b75c16c16ee15bf2943de95c3f8dae0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-03-18 14:42:01 +0100

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,16 +1,37 @@
+import {v4 as uuid} from "uuid";
+
 class TodoApp {
-  constructor({uiHandler}) {
+  constructor({UIHandler}) {
     this.todoTasks = [];
 
-    this.uiHandler = uiHandler(this);
+    this.uiHandler = new UIHandler(this);
   }
 
-  add(todo) {
+  createNewTodo = (content) => {
+    const time = new Date();
+    const id = uuid();
+    const timeString = new Intl.DateTimeFormat("hu-HU", {
+      dateStyle: "short",
+      timeStyle: "short",
+    }).format(time);
+    const newTodo = {
+      id,
+      content,
+      time: timeString,
+      active: true,
+    };
+    this.addTodo(newTodo);
+  };
+
+  addTodo(todo) {
     this.todoTasks.push(todo);
   }
 
-  remove(todo) {
-    this.todoTasks = this.todoTasks.filter((t) => t !== todo);
+  removeTodo(todoId) {
+    console.log("todoId: ", todoId);
+    console.log("this.todoTasks: ", this.todoTasks);
+    this.todoTasks = this.todoTasks.filter((todo) => todo.id !== todoId);
+    console.log("this.todoTasks: ", this.todoTasks);
   }
 
   getTodos() {
