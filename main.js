fe6d5d1fdcc4e7ece02f5657352960d1068571fc
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-03-18 15:45:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import './style.css'
 import TodoApp from "./src/TodoApp";
 import UIHandler from "./src/UIHandler.js";
+import LocalStorageHandler from "./src/LocalStorageHandler";
 
 const app = new TodoApp({
   UIHandler: UIHandler,
+  StorageHandler: LocalStorageHandler,
 });
