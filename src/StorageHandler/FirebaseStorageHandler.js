1882a9d60e6c27d524e6471c9e7e57ffd235a9ce
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-04-19 20:11:03 +0200

    Update FirebaseStorageHandler.js

--- src/StorageHandler/FirebaseStorageHandler.js
+++ src/StorageHandler/FirebaseStorageHandler.js
@@ -1,22 +1,79 @@
 import AbstractStorageHandler from "./StorageHandler.Abstract.js";
-import FirebaseApp from "../FirebaseApp/FirebaseApp.js";
+// import FirebaseApp from "../FirebaseApp/FirebaseApp.js";
+import {initializeApp} from "firebase/app";
+
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  doc,
+  setDoc,
+  where,
+} from "firebase/firestore";
 
 class FirebaseStorageHandler extends AbstractStorageHandler {
+  #todos;
+  #dbName;
+
   constructor(app) {
     super(app);
+    this.#todos = [];
 
-    this.firebaseApp = FirebaseApp.getInstance;
-    this.db = this.firebaseApp.db;
+    this.#dbName = "todos";
+
+    this.firebaseConfig = {
+      apiKey: "AIzaSyApxout8xT1PmAHAWk7Q-MAvnumqPjIUUk",
+      authDomain: "gabbenos-todo.firebaseapp.com",
+      databaseURL:
+        "https://gabbenos-todo-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "gabbenos-todo",
+      storageBucket: "gabbenos-todo.appspot.com",
+      messagingSenderId: "522421742349",
+      appId: "1:522421742349:web:aa142e679b9f1e457a5e44",
+    };
+
+    initializeApp(this.firebaseConfig);
+
+    // this.firebaseApp = FirebaseApp.getInstance;
+
+    this.db = getFirestore();
+    this.subscribeToTodos();
   }
 
-  saveToStorage(newTodos) {}
+  subscribeToTodos() {
+    const q = query(collection(this.db, this.#dbName), orderBy("timestamp", "desc"));
+    this.todoListener = onSnapshot(q, (snaps) => {
+      this.todos = snaps.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      console.log(this.todos)
+    });
+  }
+
+  unsubscribeFromTodos() {
+    if (this.todoListener != null) {
+      this.todoListener();
+      this.todoListener = null;
+    }
+  }
+
+  saveToStorage(newTodos) {
+    addDoc(collection(db, this.#dbName), newTodos);
+  }
 
   loadFromStorage() {}
 
   // CRUD - Creat, Read, Update, Delete
-  createTodo(todo) {}
+  createTodo(todo) {
+    addDoc(collection(this.db, this.#dbName), todo);
+  }
 
-  getAllTodos() {}
+  getAllTodos() {
+    return this.#todos;
+  }
 
   getOneTodo(id) {}
 
