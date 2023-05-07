3dea3efbe5acbed53a2b38c3026974a02d29c751
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-07 14:25:48 +0200

    Update TodoList.jsx

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,10 @@
 import TodoCard from "../TodoCard/TodoCard";
 
+import style from "./TodoList.module.css";
+
 const TodoList = ({todos, setTodos}) => {
   return (
-    <div>
+    <div className={style.todoList}>
       {todos.map((todo) => (
         <TodoCard key={todo.id} todo={todo} setTodos={setTodos} />
       ))}
