7cc4c4dcb4dfc8114db2904945e0ded674095b0d
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-07 13:30:29 +0200

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,43 @@
+import {useState} from "react";
+
+import style from "./Modal.module.css";
+
+const Modal = ({open, setOpen, title, children, handleOk, handleCancel}) => {
+  if (open === false) return <></>;
+
+  return (
+    <div className={style.modalCover}>
+      <div className={style.modal}>
+        <div className={style.modalHeader}>
+          <h2 className={style.modalTitle}>{title}</h2>
+          <button className={style.closeModal} onClick={() => setOpen(false)}>
+            X
+          </button>
+        </div>
+        <div className={style.modalBody}>{children}</div>
+        <div className={style.modalFooter}>
+          <button
+            className={style.footerOk}
+            onClick={() => {
+              handleOk && handleOk();
+              setOpen(false);
+            }}
+          >
+            Save
+          </button>
+          <button
+            className={style.footerCancel}
+            onClick={() => {
+              handleCancel && handleCancel();
+              setOpen(false);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
