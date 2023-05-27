30259e94c93bfddb9118086d5dbe9d7aed9042f1
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-27 15:53:20 +0200

    Create apiMethods.js

---
+++ src/api/apiMethods.js
@@ -0,0 +1,11 @@
+import {requestHeaders} from "./requestHeaders";
+
+export const postMethod = {
+  method: "POST",
+  headers: requestHeaders,
+};
+
+export const getMethod = {
+  method: "GET",
+  headers: requestHeaders,
+};
