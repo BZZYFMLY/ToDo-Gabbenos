d411a4e25f46226f5d32e5ac0bdf3262f374cc09
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-27 15:53:32 +0200

    Update TodoCard.jsx

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,15 +1,24 @@
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 import Modal from "../Modal/Modal";
 
-const TodoCard = ({todo, setTodos}) => {
+import {TodoContext} from "../../App";
+
+import {apiEndpoints} from "../../api/apiEndpoints";
+import {postMethod} from "../../api/apiMethods";
+
+import styles from "./TodoCard.module.css";
+
+const TodoCard = ({todo}) => {
+  const {setTodos, baseURL} = useContext(TodoContext);
+
   const [modalOpen, setModalOpen] = useState(false);
   const [editContent, setEditContent] = useState("");
 
   const handleDelete = () => {
-    setTodos((prev) =>
-      prev.filter((todoFromList) => todoFromList.id !== todo.id),
-    );
+    fetch(baseURL + apiEndpoints.deleteTodo, {...postMethod, body: todo.id})
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
   };
 
   const handledit = () => {
@@ -18,37 +27,52 @@ const TodoCard = ({todo, setTodos}) => {
   };
 
   const handleDone = () => {
-    setTodos((prev) => {
-      return prev.map((todoFromList) => {
-        return todoFromList.id === todo.id
-          ? {...todoFromList, done: !todoFromList?.done}
-          : todoFromList;
-      });
-    });
+    const newTodo = {...todo, done: !todo.done};
+
+    fetch(baseURL + apiEndpoints.updateTodo, {
+      ...postMethod,
+      body: JSON.stringify(newTodo),
+    })
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
   };
 
   const handleEditContent = (e) => setEditContent(e.target.value);
 
   const handleSave = () => {
-    setTodos((prev) => {
-      return prev.map((todoFromList) => {
-        return todoFromList.id === todo.id
-          ? {...todoFromList, content: editContent}
-          : todoFromList;
-      });
-    });
+    const newTodo = {...todo, content: editContent};
+    fetch(baseURL + apiEndpoints.updateTodo, {
+      ...postMethod,
+      body: JSON.stringify(newTodo),
+    })
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
   };
+
+  const urgent =
+    new Date().getTime() - new Date(todo?.date).getTime() >
+    5 * 24 * 60 * 60 * 1000;
 
   return (
     <>
-      <li>
-        <h2>{todo?.content}</h2>
-        <p>{todo?.done ? "Completed" : "Not Completed"}</p>
-        <p>{todo?.date}</p>
+      <li className={styles.todoCard}>
+        <h2 className={styles.cardTitle}>{todo?.content}</h2>
+        <p className={!todo?.done ? styles.activeTodo : styles.passiveTodo}>
+          {todo?.done ? "Completed" : "Not Completed"}
+        </p>
+        <p className={urgent ? styles.urgentTodo : null}>{todo?.date}</p>
         <p>{todo?.id}</p>
-        <button onClick={handleDelete}>Delete</button>
-        <button onClick={handledit}>Edit</button>
-        <button onClick={handleDone}>Done</button>
+        <div className={styles.btnContainer}>
+          <button className={styles.btn} onClick={handleDelete}>
+            Delete
+          </button>
+          <button className={styles.btn} onClick={handledit}>
+            Edit
+          </button>
+          <button className={styles.btn} onClick={handleDone}>
+            Done
+          </button>
+        </div>
       </li>
       <Modal
         handleOk={handleSave}
