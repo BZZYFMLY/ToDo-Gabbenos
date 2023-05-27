f567489cf84fc94b597d4d7039c4b127e964b5c5
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-27 15:53:37 +0200

    Update TodoList.jsx

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,21 @@
+import {useContext} from "react";
 import TodoCard from "../TodoCard/TodoCard";
 
-const TodoList = ({todos, setTodos}) => {
+import {TodoContext} from "../../App";
+
+import styles from "./TodoList.module.css";
+
+const TodoList = () => {
+  const {todos} = useContext(TodoContext);
+
   return (
-    <div>
-      {todos.map((todo) => (
-        <TodoCard key={todo.id} todo={todo} setTodos={setTodos} />
-      ))}
+    <div className="Todo-list">
+      <h2 className={styles.listTitle}> Todos </h2>
+      <ul className={styles.todoList}>
+        {todos?.map((todo) => (
+          <TodoCard key={todo.id} todo={todo} />
+        ))}
+      </ul>
     </div>
   );
 };
